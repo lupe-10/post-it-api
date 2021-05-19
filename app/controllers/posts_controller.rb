244f27967b785506f9936870cbbf1a5c784fe1a9
Lucia diez <lupe.diez@gmail.com>
2021-05-19 10:48:55 -0300

    posts controller actions and posts routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+  before_action :set_character, only: %i[show create update delete]
+  before_action :authorize_request, except: %i[index]
+
+  def index
+    if params[:query].present?
+    @posts = Post.where("title @@ ?", "%#{params[:query]}%")
+    # hacer el filtro
+    else
+    @posts = Post.all
+  end
+
+  def show 
+    # si no existe devolver mensaje de error
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render ?
+    else
+      render_error
+    end
+  end
+
+  def update
+    @post = Post.update(post_params)
+    if @post.save
+      render ?
+    else
+      render_error
+      # cuando no lo encunetra
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      head(:ok)
+    else
+      render_error
+      # cuando no lo encuentra
+    end
+  end
+
+  private
+
+  def set_post
+    @post = post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :contents, :user_id)
+  end
+
+  def render_error
+  render json: { errors: object.errors.full_messages },
+                  status: :unprocessable_entity
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, param: :_username
+  resources :posts, only: %i[index show create update destroy]
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
 end
